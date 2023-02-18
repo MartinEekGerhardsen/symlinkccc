53858fe58d29b22387d5ea79184299802c941a1d
Martin Eek Gerhardsen <Martin.Eek.Gerhardsen@sintef.no>
2023-02-18 23:45:32 +0100

    handling some edge cases

--- build.rs
+++ build.rs
@@ -1,19 +1,21 @@
-use const_gen::*;
+use const_gen::{const_declaration, CompileConst};
 use std::{env, fs, path::Path};
 
 fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("const_config.rs");
+    if let Some(out_dir) = env::var_os("OUT_DIR") {
+        let dest_path = Path::new(&out_dir).join("const_config.rs");
 
-    let const_declarations = vec![
-        const_declaration!(pub METADATA_DIR_NAME = ".catkin_tools"),
-        const_declaration!(pub ROSPACK_COMMAND = "rospack"),
-        const_declaration!(pub BUILD_IGNORE_DIRS = ["build", "catkin_tools_prebuild"]),
-        const_declaration!(pub COMPILE_COMMANDS_NAME = "compile_commands.json"),
-    ]
-    .join("\n");
+        let const_declarations = vec![
+            const_declaration!(pub METADATA_DIR_NAME = ".catkin_tools"),
+            const_declaration!(pub ROSPACK_COMMAND = "rospack"),
+            const_declaration!(pub BUILD_IGNORE_DIRS = ["build", "catkin_tools_prebuild"]),
+            const_declaration!(pub COMPILE_COMMANDS_NAME = "compile_commands.json"),
+        ]
+        .join("\n");
 
-    fs::write(dest_path, const_declarations).unwrap();
-
+        if let Err(err) = fs::write(dest_path, const_declarations) {
+            println!("Cannot write const file: {err}");
+        }
+    }
     println!("cargo:rerun-if-changed=build.rs");
 }
