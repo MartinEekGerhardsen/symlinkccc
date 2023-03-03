7c4d07eb91598482d6e65fe36c26fcbfcd73d2ab
Martin Eek Gerhardsen <Martin.Eek.Gerhardsen@sintef.no>
2023-03-03 14:34:43 +0100

    more clippy

--- src/build_package.rs
+++ src/build_package.rs
@@ -5,10 +5,10 @@ use crate::{
     paths::structs::{Build, BuildPackage, Workspace},
 };
 
-fn valid_path_buf_to_str(path: &std::path::PathBuf) -> Option<String> {
-    path.file_name().map_or(None, |file_name| {
-        file_name.to_str().map(std::string::ToString::to_string)
-    })
+fn path_to_string<P: AsRef<std::path::Path>>(path: P) -> Option<String> {
+    path.as_ref()
+        .file_name()
+        .and_then(|file_name| file_name.to_str().map(std::string::ToString::to_string))
 }
 
 pub fn get_all_build_package_paths(workspace: &Workspace) -> HashMap<Package, BuildPackage> {
@@ -22,7 +22,7 @@ pub fn get_all_build_package_paths(workspace: &Workspace) -> HashMap<Package, Bu
             .filter_map(std::result::Result::ok)
             .map(|e| e.path())
             .filter(|p| p.exists() && p.is_dir())
-            .filter_map(|p| valid_path_buf_to_str(&p))
+            .filter_map(path_to_string)
             .filter(|s| !crate::config::BUILD_IGNORE_DIRS.contains(&s.as_str()))
             .map(|s| (Package(s.clone()), BuildPackage(build_path.join(s))))
             .collect()
